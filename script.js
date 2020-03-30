8cc22075b22bbc85a44744ddd06533207ffa35d3
PzOng <pangszee.ong@gmail.com>
2020-03-30 20:28:28 +1030

    lat lon

---
+++ script.js
@@ -0,0 +1,55 @@
+var lat, lon;
+var apikey ='5e799cbc4834793851ed4eb3fbe95228';
+var options = {
+    enableHighAccuracy:true,
+    timeout: 5000,
+    maximumAge:0
+};
+
+navigator.geolocation.getCurrentPosition(success,error,options);
+
+  
+function success(pos){
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+    console.log("Latitude = " + lat + " " + "Longitude = " + lon);
+    info(lat,lon);
+  }
+  
+function error(error){
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        console.log("An unknown error occurred.")
+        break;
+    }
+  }
+
+
+function info(lat, lon){
+
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+apikey;
+    $.ajax({
+          url: queryURL,
+          method: "GET",
+          success: function(response){
+            var result = response;
+
+            console.log(result)
+                   
+          },
+          error: function(){
+            var msg = "City Not found";
+            console.log(msg);
+          }
+
+})
+}
